67ee2e1c5d8900cf391447bec16dbd850075a32a
Zehra <Zehra@users.noreply.github.com>
2023-07-22 22:33:11 -0400

    added testing unit

---
+++ src/en/Nightcorenovels.lua
@@ -0,0 +1,16 @@
+return Require("Madara")("https://nightcorenovels.com", {
+	id = 7474,
+	name = "Nightcore Novels",
+	imageURL = "https://nightcorenovels.com/wp-content/uploads/2023/03/434d12f09f54479bd346b9937b32c5f1.png",
+
+	-- defaults values
+	latestNovelSel = "div.col-12.col-md-6.badge-pos-2",
+	--searchNovelSel = "div.page-item-detail.text",
+	novelListingURLPath = "",
+	hasSearch = false,
+	--ajaxUsesFormData = true,
+	genres = {}
+
+})
+
+
